2fb9180502309e3577bd17cf6d72499aa0590927
mohammadsaleh <mohammadsaleh.sayari@hotmail.com>
2014-05-16 23:22:00 +0430

    Fixed: some errors in the admin_index and front page

--- Controller/Component/AparnicMetaboxComponent.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class AparnicMetaboxComponent extends Component{
-    
-    public function startup(Controller $controller) {
-        $currentAction = Inflector::camelize($controller->request->params['controller']) . '/' . $controller->request->params['action'];
-        Configure::write('currentAction', $currentAction);
-    }
-}
